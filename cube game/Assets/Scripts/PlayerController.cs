65a566684484980c923c2fe9cb20d6291985af8b
nil17593 <navgekar18@gmail.com>
2021-07-31 17:04:12 +0530

    by physics force player can move now.

---
+++ cube game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 1000*Time.deltaTime);
+    }
+}
