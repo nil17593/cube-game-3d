be8f1f2fd5522b1e4285a366fd1fb6adbd63fc3d
nil17593 <navgekar18@gmail.com>
2021-07-31 18:36:54 +0530

    camera now follow the player

---
+++ cube game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float smoothFactor;
+
+    void FollowPlayer()
+    {
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothedposition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.deltaTime);
+        transform.position = targetPosition;
+    }
+    private void LateUpdate()
+    {
+        FollowPlayer();
+    }
+}
--- cube game/Assets/Scripts/PlayerController.cs
+++ cube game/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,47 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private float xAxisForce;
+    [SerializeField] private float forwardForce;
+    bool leftKey, rightKey;
+
+
+    private void Update()
+    {
+        //leftKey = Input.GetKeyDown(KeyCode.A);
+        //rightKey= Input.GetKeyDown(KeyCode.D);
+
+        leftKey = Input.GetKey("a");
+        rightKey = Input.GetKey("d");
+        Movement();
+    }
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 1000*Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce*Time.deltaTime);
+
+        //add force to the x axis
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    rb.AddForce(xAxisForce * Time.deltaTime, 0, 0);
+        //}
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    rb.AddForce(-xAxisForce * Time.deltaTime, 0, 0);
+        //}
+
     }
+    void Movement()
+    {
+        if (rightKey)
+        {
+            rb.AddForce(xAxisForce * Time.deltaTime, 0, 0);
+        }
+
+        if (leftKey)
+        {
+            rb.AddForce(-xAxisForce * Time.deltaTime, 0, 0);
+        }
+
+    }
+
 }
