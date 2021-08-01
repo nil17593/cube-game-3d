8e7ec9d772e34163c36b51665760195ed7ab8d4b
nil17593 <navgekar18@gmail.com>
2021-08-01 22:24:51 +0530

    Two Playable Levels

---
+++ cube game/Assets/LevelWinController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelWinController : MonoBehaviour
+{
+    [SerializeField] GameObject levelcomlete;
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] Button buttonMenu;
+    [SerializeField] Button buttonNextLevel;
+    [SerializeField] string lobby;
+
+    private void Awake()
+    {
+        buttonMenu.onClick.AddListener(LoadLobby);
+        buttonNextLevel.onClick.AddListener(LoadNextLevel);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void LoadLobby()
+    {
+        SceneManager.LoadScene(lobby);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            levelcomlete.SetActive(true);
+            playerController.enabled = false;
+        }
+    }
+}
